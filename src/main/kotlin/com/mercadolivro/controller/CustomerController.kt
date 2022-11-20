f611b58069424a626138e93b05098b75cc949e49
Victor Neves <victorneveshs@gmail.com>
2022-11-20 20:23:33 -0300

    base for pratice

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,43 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("customer")
+class CustomerController (
+    val customerService: CustomerService
+){
+    @GetMapping
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+       return customerService.getAll(name)
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        customerService.create(customer)
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customerService.getCustomer(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+            customerService.update(id, customer)
+        }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customerService.delete(id)
+    }
+
+}
